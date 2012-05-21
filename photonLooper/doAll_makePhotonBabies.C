b4613862bf21c810a84f45abd310b5aa7d974b53
olivito <olivito>
2012-05-21 21:17:18 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-00-05'.

--- photonLooper/doAll_makePhotonBabies.C
+++
@@ -1,8 +0,0 @@
-{
-  gROOT->ProcessLine(".L histtools.C+");
-  gROOT->ProcessLine(".L runMakePhotonBabies.C+");
-  
-  //--photon+jets---
-  //runMakePhotonBabies("Photon"                , true  );   // Photon data
-  runMakePhotonBabies("DoubleElectron"        , true  );   // DoubleElectron data
-}
